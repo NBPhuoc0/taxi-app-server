d7d4d7ffcf1977f069c04bca9e21e2064e792b5a
NBPhuoc <phcnguyenba@gmail.com>
2023-09-22 13:02:17 +0700

    update for azure web app

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './utils/swagger';
 import helmet from 'helmet';
-import { AzureSocketIO } from './chat/adapters/azure-pubsub.adapter';
+// import { AzureSocketIO } from './chat/adapters/azure-pubsub.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -21,7 +21,7 @@ async function bootstrap() {
   setupSwagger(app)
   app.use(helmet())
 
-  app.useWebSocketAdapter(new AzureSocketIO(app))
+  //app.useWebSocketAdapter(new AzureSocketIO(app))
 
   await app.listen(3000);
   logger.log(`API Documentation available at localhost:3000`);
