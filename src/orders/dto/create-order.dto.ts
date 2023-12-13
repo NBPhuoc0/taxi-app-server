de04b1cdb87a1fffb28fb42e2e770023d8438dca
NBPhuoc <phcnguyenba@gmail.com>
2023-12-13 17:09:12 +0700

    update ordermodel

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,7 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
+import { location } from "src/utils/interface/location.interface";
 
 export class CreateOrderDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    vehicle_type: string;
 
     @ApiProperty()
     @IsNotEmpty()
@@ -13,21 +17,15 @@ export class CreateOrderDto {
 
     @ApiProperty()
     @IsNotEmpty()
-    orderTotal: number;
-
-    @ApiProperty()
-    @IsNotEmpty()
-    source_location: {
-        lat: number;
-        long: number;
-    };
+    source_location: location;
     
     @ApiProperty()
     @IsNotEmpty()
-    destination_location: {
-        lat: number;
-        long: number;
-    };
+    destination_location: location;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    orderTotal: number;
 
     @ApiProperty()
     @IsNotEmpty()
